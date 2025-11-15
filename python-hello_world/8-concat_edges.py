e8618e3d18327360dfa505133e86483833d0836a
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-15 16:48:01 +0000

    edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming '\n' language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[111:116] + str[:6])
+str = ("Python is an interpreted, interactive, object-oriented programming " \
+ "language that combines remarkable power with very clear syntax")
+print(str[39:66] + str[106:112] + str[:6])
