618d5daed4797a17dd0ee4a7aaacd95fa8fc3c55
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-15 14:44:56 +0000

    reverse

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-for i in range(25, -1, -1):
-    print("{}".format(chr(122 - i) if i % 2 == 0 else chr(90 - i)), end="")
+#!/usr/bin/env python3
+for i in range(26):
+    print("{:c}".format((122 - i) if i % 2 == 0 else (122 - i) - 32), end="")
