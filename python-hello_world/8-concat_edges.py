8786032d9e87f51f700193e349a1f5b9cb48ac82
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-15 16:18:58 +0000

    edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# Print only the object-oriented part
-print(str[42:71] + " " + str[:6])
+# Print only the required part directly from str
+print(str[42:71] + str[0:6])
