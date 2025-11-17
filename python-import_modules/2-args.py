0c7e0bc1174f6e53f8cdc9780235c6b9646d2fed
theaskrv <askerovsahil5757@gmail.com>
2025-11-17 13:25:58 +0000

    modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 import sys
 
 if __name__ == "__main__":
-    args = sys.argv[1:]  # 1-ci elementdən başlayaraq bütün arqumentləri götürürük, script adı çıxılır
-    num_args = len(args)  # arqumentlərin sayını hesablayırıq
+    args = sys.argv[1:]  
+    num_args = len(args) 
 
     if num_args == 0:
-        print("0 arguments.")  # heç bir arqument yoxdursa
+        print("0 arguments.")  
     else:
-        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")  # 1 arqument varsa "argument", bir neçə varsa "arguments"
-        for i, arg in enumerate(args, start=1):  # arqumentləri nömrələyərək dövrəyə alırıq
-            print(f"{i}: {arg}")  # 1: Hello, 2: Welcome və s.
+        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")  
+        for i, arg in enumerate(args, start=1):  
+            print(f"{i}: {arg}") .
