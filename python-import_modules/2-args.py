8f42eae4858a84a9170ca7b4cfda8af289cec1a4
theaskrv <askerovsahil5757@gmail.com>
2025-11-17 13:29:50 +0000

    modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,11 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv[1:]  
-    num_args = len(args) 
+    num_args = len(args)  
 
     if num_args == 0:
-        print("0 arguments.")  
+        print("0 arguments.")
     else:
         print(f"{num_args} argument{'s' if num_args > 1 else ''}:")  
-        for i, arg in enumerate(args, start=1):  
-            print(f"{i}: {arg}") .
+        for i, arg in enumerate(args, start=1):
+            print(f"{i}: {arg}")  
