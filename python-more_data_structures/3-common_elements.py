68efb15d39d90c5de5217298f66fd9ff940208d1
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-19 16:15:08 +0000

    python

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    same = set()
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                same.add(i)
+    return same
