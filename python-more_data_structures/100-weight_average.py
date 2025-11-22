b78410ffeaa381e93a027cac597f65724c1ef3a1
theaskrv57 <askerovsahil5757>
2025-11-22 15:38:30 +0000

    avarage

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list or len(my_list) == 0:
+        return 0
+
+    total_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    return total_score / total_weight
