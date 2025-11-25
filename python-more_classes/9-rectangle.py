009793cde288e685357142957be11fd11667e92d
theaskrv57 <askerovsahiil5757@gmail.com>
2025-11-25 10:26:00 +0000

    classes

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -52,27 +52,4 @@ class Rectangle:
 
     def __str__(self):
         if self.width == 0 or self.height == 0:
-            return ""
-        return "\n".join([str(self.print_symbol) * self.width
-                          for _ in range(self.height)])
-
-    def __repr__(self):
-        return f"Rectangle({self.width}, {self.height})"
-
-    def __del__(self):
-        print("Bye rectangle...")
-        type(self).number_of_instances -= 1
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """Return a new Rectangle instance with width == h
+            retu
