afb1d941f55b94a78f7059ec978eed1d8f12ee45
theaskrv57 <askerovsahiil5757@gmail.com>
2025-11-25 09:39:19 +0000

    classes

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -5,17 +5,21 @@ area, perimeter, and string representation using '#'."""
 
 class Rectangle:
 
-    def __init__(self, width = 0, height = 0):
+""" skmdf"""
 
+    def __init__(self, width = 0, height = 0):
+""" skmdf"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+""" skmdf"""
         return self.__width
 
     @width.setter
     def width(self, value):
+""" skmdf"""
 
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -27,11 +31,12 @@ class Rectangle:
 
     @property
     def height(self):
+""" skmdf"""
         return self.__height
 
     @height.setter
     def height(self, value):
-
+""" skmdf"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
 
@@ -41,9 +46,11 @@ class Rectangle:
 
 
     def area(self):
+""" skmdf"""
         return self.__width * self.__height
 
     def perimeter(self):
+""" skmdf"""
         if self.__width == 0 or self.__height == 0:
             return 0
 
