34e7dfb57811900fbb992983d8d610603a2ae2a4
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-27 13:53:09 +0000

    classes

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""salam"""
+def read_file(filename=""):
+    """Read a UTF-8 text file and print its content to stdout."""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
