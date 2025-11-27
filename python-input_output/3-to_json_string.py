fdac1b9bd5bf5840a256b9ce249ab12ea10e71ec
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-27 14:02:07 +0000

    classes

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""salam"""
+
+
+
+import json
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a Python object (string)."""
+    return json.dumps(my_obj)
