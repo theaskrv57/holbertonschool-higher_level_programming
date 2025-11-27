8377f502dfc5eb209c521d75afa2061d15019dd1
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-27 14:07:39 +0000

    classes

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""salam"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Create a Python object from a JSON file."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
