37d441159b0782391e8326285fdaadde4e0d5957
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-28 10:37:53 +0000

    programming

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,17 @@
 -- salam
-SELECT column_name AS Field,
-       column_type AS Type,
-       is_nullable AS Null,
-       column_key AS Key,
-       column_default AS Default,
-       extra AS Extra
+SELECT CONCAT(
+    'CREATE TABLE `', table_name, '` (\n',
+    GROUP_CONCAT(
+        '  `', column_name, '` ', column_type,
+        IF(is_nullable='NO',' NOT NULL',''),
+        IF(extra<>'',' ',extra),
+        IF(column_default IS NOT NULL, CONCAT(' DEFAULT \'', column_default, '\''),'')
+        ORDER BY ordinal_position
+        SEPARATOR ',\n'
+    ),
+    ',\n  PRIMARY KEY(`id`)\n',
+    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+) AS create_table_statement
 FROM information_schema.columns
-WHERE table_schema = DATABASE() AND table_name = 'first_table';
+WHERE table_schema = DATABASE() AND table_name = 'first_table'
+GROUP BY table_name;
