b6f5bd62a6c980316ebf6933c72eb2d5ce4d8e92
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-28 10:39:29 +0000

    programming

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,3 @@
 -- salam
-SELECT CONCAT(
-    'CREATE TABLE `', table_name, '` (\n',
-    GROUP_CONCAT(
-        '  `', column_name, '` ', column_type,
-        IF(is_nullable='NO',' NOT NULL',''),
-        IF(extra<>'',' ',extra),
-        IF(column_default IS NOT NULL, CONCAT(' DEFAULT \'', column_default, '\''),'')
-        ORDER BY ordinal_position
-        SEPARATOR ',\n'
-    ),
-    ',\n  PRIMARY KEY(`id`)\n',
-    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
-) AS create_table_statement
-FROM information_schema.columns
-WHERE table_schema = DATABASE() AND table_name = 'first_table'
-GROUP BY table_name;
+-- Bu bütün sətirləri qaytaracaq, çünki hazırda hbtn_0c_0 aktivdir
+SELECT * FROM first_table WHERE DATABASE() = 'hbtn_0c_0';
