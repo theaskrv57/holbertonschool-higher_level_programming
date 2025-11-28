ac20dbb12b22519e774f52312eda4aae48ef4711
theaskrv57 <askerovsahil5757@gmail.com>
2025-11-28 10:36:10 +0000

    programming

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,9 @@
 -- salam
-SELECT * FROM first_table WHERE DATABASE='hbtn_0c_0';
+SELECT column_name AS Field,
+       column_type AS Type,
+       is_nullable AS Null,
+       column_key AS Key,
+       column_default AS Default,
+       extra AS Extra
+FROM information_schema.columns
+WHERE table_schema = DATABASE() AND table_name = 'first_table';
