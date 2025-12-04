ecd0294c5a498f28c081a01e3c7b634a1bdb7bba
theaskrv57 <askerovsahil5757@gmail.com>
2025-12-04 11:56:08 +0000

    network

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,27 +1,32 @@
 class SimpleAPIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Root endpoint
         if self.path == "/":
-            self.send_response(200)  # HTTP status code 200 OK
+            self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-        # /data endpoint
         elif self.path == "/data":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode())  # dict -> JSON string
-        # /status endpoint
+            self.wfile.write(json.dumps(data).encode())
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
-        # Undefined endpoint
         else:
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Endpoint not found")
+
+def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Serving on port {port}...")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
