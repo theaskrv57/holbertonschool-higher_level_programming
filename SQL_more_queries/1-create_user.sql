a61a137d9fefabb3de7192d80986ed41438fd0f5
theaskrv57 <askerovsahil5757@gmail.com>
2026-02-12 06:58:42 -0500

    cyber

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Create user user_0d_1 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
